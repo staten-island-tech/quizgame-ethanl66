44d53c74f25f6b47d2d55ec93d67c2df37594064
Ehan <ethanliang405@gmail.com>
2021-04-12 20:40:23 -0400

    game.js loading in results.html

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -2,6 +2,8 @@ const DOMSelectors = {
   displayContainer: document.querySelector(".container"),
   questions: document.querySelector(".question"),
   gameBox: document.querySelector("#game"),
+  card: document.querySelector(".card"),
+  resultsBox: document.querySelector(".results-home"),
 };
 
 export { DOMSelectors };
--- src/js/results.js
+++ src/js/results.js
@@ -1,3 +1,6 @@
+import { DOMSelectors } from "./DOM";
+import { finalArrayExport } from "./game";
+
 //HEIGHT OF BACKGROUND IMAGE SAME AS #HOME
 const resultsHome = document.getElementById("results-home");
 console.log(resultsHome);
@@ -7,3 +10,28 @@ console.log(resultsHomeHeight);
 const backgroundImage = document.getElementById("bg-image");
 backgroundImage.style.height = resultsHomeHeight + 175 + "px";
 backgroundImage.style.minHeight = "100%";
+
+//DISPLAY RESULTS
+displayResults = function () {
+  //DOMSelectors.card.innerHTML = "";
+  DOMSelectors.resultsBox.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card" id="card">
+    <h3 class="title-name">${finalArrayExport[0].title}</h3>
+    <div class="card-data">
+        <div class="anime-data-div">
+            <p class="synopsis anime-data">${finalArrayExport[0].synopsis}</p><br>
+            <a target="_blank" rel="noopener noreferrer" href="${finalArrayExport[0].link}" class="mal-link anime-data">Link to MAL</a> 
+        </div>
+        <div class="anime-img-div">
+            <img src="${finalArrayExport[0].img_url}" alt="${finalArrayExport[0].title} cover art"
+                class="anime-img">
+            <p class="genre anime-data">Genres: ${finalArrayExport[0].genre}</p>
+            <p class="air-date anime-data">Air date: ${finalArrayExport[0].aired}</p>
+            <p class="episodes anime-data">Episodes: ${finalArrayExport[0].episodes}</p>
+            <p class="mal-score anime-data">MAL Score: ${finalArrayExport[0].score}</p>
+        </div>
+    </div>
+</div>`
+  );
+};
